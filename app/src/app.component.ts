6c7ac7a0123ae8ea44af2c3be48f1cb4c0d9309a
Levi Fuller <lfuller941@gmail.com>
2016-01-19 23:11:18 -0700

    Initial Commit

---
+++ app/src/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    moduleId: 'app/src/',
+    selector: 'my-app',
+    templateUrl: 'app.view.html'
+})
+export class AppComponent {
+    
+}
